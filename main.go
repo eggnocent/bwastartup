dbc2e469b3a85ea50c2d5bf3f85843251a19a1f2
eggnocent <egiwiratamaaa@gmail.com>
2024-09-18 15:21:42 +0700

    add route for loging

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bwastartup/handler"
 	"bwastartup/users"
-	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -22,25 +21,13 @@ func main() {
 	userRepository := users.NewRepository(db)
 	userService := users.NewService(userRepository)
 
-	input := users.LoginInput{
-		Email:    "egi@sysbraykr.com",
-		Password: "1234mmj5",
-	}
-
-	user, err := userService.Login(input)
-	if err != nil {
-		fmt.Println("terjadi kesalahan")
-		fmt.Println(err.Error())
-	}
-	fmt.Println(user.Email)
-	fmt.Println(user.Name)
-
 	userHandler := handler.NewUserHandler(userService)
 
 	router := gin.Default()
 
 	api := router.Group("/api/v1")
 	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/session", userHandler.Login)
 
 	router.Run()
 

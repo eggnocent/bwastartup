609fd9a3cbc1ee115f0c12269650ecbc6c90045b
eggnocent <egiwiratamaaa@gmail.com>
2024-09-24 10:34:26 +0700

    add repo

--- campaign/repository.go
+++ campaign/repository.go
@@ -8,6 +8,7 @@ type Repository interface {
 	FindAll() ([]Campaign, error)
 	FindByUserID(userID int) ([]Campaign, error)
 	FindByID(userID int) (Campaign, error)
+	Save(campaign Campaign) (Campaign, error)
 }
 
 type repository struct {
@@ -44,5 +45,14 @@ func (r *repository) FindByID(userID int) (Campaign, error) {
 	if err != nil {
 		return campaign, err
 	}
+	return campaign, nil
+}
+
+func (r *repository) Save(campaign Campaign) (Campaign, error) {
+	err := r.db.Create(&campaign).Error
+	if err != nil {
+		return campaign, err
+	}
+
 	return campaign, nil
 }

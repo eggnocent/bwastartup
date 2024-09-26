53b0ca157e4c1ff2788bb1e74ad9508ff2869903
eggnocent <egiwiratamaaa@gmail.com>
2024-09-26 11:09:07 +0700

    add insert for transaction data

--- db/migrations/20240926034805_input_transaction.up.sql
+++ db/migrations/20240926034805_input_transaction.up.sql
@@ -1,6 +1,6 @@
 INSERT INTO transactions (campaign_id, user_id, amount, status)
 VALUES
-    (9, 17, 500000, 'success'),
+    (9, 18, 500000, 'success'),
     (9, 16, 300000, 'pending'),
     (11, 20, 1000000, 'success'),
     (11, 18, 750000, 'failed'),

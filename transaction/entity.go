5b76ad2b54c4267f267306f39138c4c36c5b7f3c
eggnocent <egiwiratamaaa@gmail.com>
2024-09-27 10:23:27 +0700

    add repo for get campaign id

--- transaction/entity.go
+++ transaction/entity.go
@@ -1,6 +1,7 @@
 package transaction
 
 import (
+	"bwastartup/campaign"
 	"bwastartup/users"
 	"time"
 )
@@ -13,6 +14,7 @@ type Transaction struct {
 	Status     string
 	Code       string
 	User       users.User
+	Campaign   campaign.Campaign
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 }
--- transaction/repository.go
+++ transaction/repository.go
@@ -10,6 +10,7 @@ type repository struct {
 
 type Repository interface {
 	GetByCampaignID(campaignID int) ([]Transaction, error)
+	GetByUserID(userID int) ([]Transaction, error)
 }
 
 func NewRepository(db *gorm.DB) *repository {
@@ -19,6 +20,16 @@ func NewRepository(db *gorm.DB) *repository {
 func (r *repository) GetByCampaignID(campaignID int) ([]Transaction, error) {
 	var transaction []Transaction
 	err := r.db.Preload("User").Where("campaign_id = ?", campaignID).Order("id desc").Find(&transaction).Error
+	if err != nil {
+		return transaction, err
+	}
+	return transaction, nil
+}
+
+func (r *repository) GetByUserID(userID int) ([]Transaction, error) {
+	var transaction []Transaction
+
+	err := r.db.Preload("Campaign.CampaignImage", "campaign_image.is_primary = 1").Where("user_id", userID).Find(&transaction).Error
 	if err != nil {
 		return transaction, err
 	}

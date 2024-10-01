6c7d33ca8768b4ee506e9f7ee577379cebd09e0e
eggnocent <egiwiratamaaa@gmail.com>
2024-10-01 16:33:53 +0700

    add input for create transaction

--- transaction/input.go
+++ transaction/input.go
@@ -1,8 +1,16 @@
 package transaction
 
-import "bwastartup/users"
+import (
+	"bwastartup/users"
+)
 
 type GetCampaignTransactionsInput struct {
 	ID   int `uri:"id" binding:"required"`
 	User users.User
+}
+
+type CreateTransactionInput struct {
+	Amount     int `json:"amount" binding:"required"`
+	CampaignID int `json:"campaign_id" binding:"required"`
+	users      users.User
 }

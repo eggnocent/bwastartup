d02c2ae569ccb6c0223c8077834875a68f8c7a09
eggnocent <egiwiratamaaa@gmail.com>
2024-10-01 16:34:25 +0700

    add service for create transaction

--- transaction/service.go
+++ transaction/service.go
@@ -13,6 +13,7 @@ type service struct {
 type Service interface {
 	GetTransactionByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
 	GetTransactionByUserID(userID int) ([]Transaction, error)
+	CreateTransaction(input CreateTransactionInput) (Transaction, error)
 }
 
 func NewService(repository Repository, campaignRepository campaign.Repository) *service {
@@ -60,4 +61,18 @@ func (s *service) GetTransactionByUserID(userID int) ([]Transaction, error) {
 		return transactions, err
 	}
 	return transactions, nil
+}
+
+func (s *service) CreateTransaction(input CreateTransactionInput) (Transaction, error) {
+	transaction := Transaction{}
+	transaction.CampaignID = input.CampaignID
+	transaction.Amount = input.Amount
+	transaction.UserID = input.users.ID
+	transaction.Status = "pending"
+
+	newTransaction, err := s.repository.Save(transaction)
+	if err != nil {
+		return transaction, err
+	}
+	return newTransaction, nil
 }
